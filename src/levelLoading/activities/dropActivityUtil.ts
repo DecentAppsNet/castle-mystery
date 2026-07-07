a242aad2d37a897f02ee958f0298d22bf40e0bde
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2026-07-07 16:26:50 -0700

    Remove debug logging.

--- src/levelLoading/activities/dropActivityUtil.ts
+++ src/levelLoading/activities/dropActivityUtil.ts
@@ -157,7 +157,6 @@ function _findDropTargetWaypoint(room:Room, activityStartTime:number,
 export function tryCreateDropActivity(activityText:string, context:ActivityContext):ItineraryEvent[]|null {
   const trimmedActivityText = activityText.trim();
   if (!trimmedActivityText.startsWith('drops ')) return null;
-  console.log(`${context.character.id} ${activityText}`);
 
   ensureTimestampIsAvailable(context.state, context.timestamp, activityText, context.timestampType);
   const activityStartTime = calcActivityStartTime(context.state, context.timestamp, context.timestampType);
